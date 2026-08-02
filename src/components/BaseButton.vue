e60813041b65e3c8d9877ac9930a2f31a33f5689
SlaveAngelovski <slave.angelovski@gmail.com>
2026-08-02 21:39:56 +0200

    feat: updating the button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,14 @@
 <template>
-  <button class="rounded-md border border-slate-300 bg-slate-100 px-3 py-2 text-sm font-medium text-slate-700 transition hover:bg-slate-200" type="button">
-    <slot />
-  </button>
+    <button :type="type"
+        class="rounded-md border border-slate-300 bg-slate-100 px-3 py-2 text-sm font-medium text-slate-700 transition hover:bg-slate-200">
+        <slot />
+    </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+withDefaults(defineProps<{
+    type?: 'button' | 'submit' | 'reset'
+}>(), {
+    type: 'button',
+})
+</script>
