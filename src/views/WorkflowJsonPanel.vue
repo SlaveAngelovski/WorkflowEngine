be90e234325ea5883695af24c1163013ca8ca133
SlaveAngelovski <slave.angelovski@gmail.com>
2026-08-02 22:09:35 +0200

    fix: set height on the json preview to 50vh

--- src/views/WorkflowJsonPanel.vue
+++ src/views/WorkflowJsonPanel.vue
@@ -1,7 +1,7 @@
 <template>
     <PanelCard title="Workflow JSON">
         <pre
-            class="max-h-80 overflow-auto rounded-md bg-slate-950 p-3 text-xs leading-6 text-slate-100">{{ formattedWorkflow }}</pre>
+            class="max-h-[50vh] overflow-auto rounded-md bg-slate-950 p-3 text-xs leading-6 text-slate-100">{{ formattedWorkflow }}</pre>
     </PanelCard>
 </template>
 
